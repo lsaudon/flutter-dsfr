f63e3b23904b26db74d77893d59adfb219047700
Jordan Chapuy <chapuyj@gmail.com>
2025-02-10 15:09:34 +0100

    feat: génération du fichier de couleurs

--- scripts/importer_palette.py
+++ scripts/importer_palette.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import requests
 from bs4 import BeautifulSoup
+import os
 
 
 # Constants
@@ -80,6 +81,53 @@ def parse_iframe(path: str) -> list[Couleur]:
     return couleurs
 
 
+def filter_unique(couleurs: list[Couleur]) -> list[Couleur]:
+    return list({couleur.nom: couleur for couleur in couleurs}.values())
+
+
+# Generation
+
+def template_dart_file() -> str:
+    return """
+import 'package:flutter/material.dart';
+
+class Couleurs {
+  const Couleurs._();
+
+  <%COULEURS%>
+}
+    """.strip()
+
+
+def dart_constants(couleur: Couleur) -> str:
+    camel_case_nom = ''.join(
+        word.capitalize()
+        if i != 0 else word for i,
+        word in enumerate(couleur.nom.split('-'))
+    )
+
+    return f"""
+    static const {camel_case_nom} = Color(0xFF{couleur.normal_hex});
+    static const {camel_case_nom}Hover = Color(0xFF{couleur.hover_hex});
+    static const {camel_case_nom}Active = Color(0xFF{couleur.active_hex});
+    """
+
+
+def generate_dart_colors(couleurs: list[Couleur]):
+    couleurs_dart = [dart_constants(couleur) for couleur in couleurs]
+    couleurs_dart_str = "".join(couleurs_dart)
+    return template_dart_file().replace("<%COULEURS%>", couleurs_dart_str)
+
+
+# File
+
+
+def write_dart_file(dart: str):
+    os.makedirs("./gen", exist_ok=True)
+    with open("./gen/couleurs.dart", "w") as file:
+        file.write(dart)
+
+
 # Main
 
 
@@ -87,12 +135,14 @@ def main():
     palette_html = fetch(DESIGN_SYSTEM_URL + PALETTE_PATH)
 
     couleurs = parse_palette(palette_html)
-    nb_couleurs = len(couleurs)
-    assert nb_couleurs == NOMBRE_DE_COULEURS
+    assert len(couleurs) == NOMBRE_DE_COULEURS
 
-    # TODO: generate dart file
+    couleurs = filter_unique(couleurs)
 
-    print(f"Fin. {nb_couleurs} couleurs importées.")
+    dart = generate_dart_colors(couleurs)
+    write_dart_file(dart)
+
+    print(f"Fin. {len(couleurs)} couleurs importées.")
 
 
 if __name__ == "__main__":
