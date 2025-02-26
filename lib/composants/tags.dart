d8756b23bd0f2f6cb0a4ed8901b707b741541127
j2sun <sunjiaji@yahoo.com>
2025-02-26 16:08:23 +0100

    feat(tag): gestion du focus (#24)
    Co-authored-by: Jiaji SUN <jiaji.sun@octo.com>

--- lib/composants/tags.dart
+++ lib/composants/tags.dart
@@ -9,6 +9,7 @@ class DsfrTag extends StatelessWidget {
     this.textColor,
     this.icon,
     this.onTap,
+    this.focusNode,
   });
 
   final IconData? icon;
@@ -16,38 +17,50 @@ class DsfrTag extends StatelessWidget {
   final GestureTapCallback? onTap;
   final Color? backgroundColor;
   final Color? textColor;
+  final FocusNode? focusNode;
 
   @override
-  Widget build(final context) => DecoratedBox(
-        decoration: ShapeDecoration(
-          color: backgroundColor ?? DsfrColorDecisions.backgroundActionLowBlueFrance(context),
-          shape: const StadiumBorder(),
-        ),
-        child: GestureDetector(
-          onTap: onTap,
-          behavior: HitTestBehavior.opaque,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 8),
-            child: Text.rich(
-              TextSpan(
-                children: [
-                  if (icon != null) ...[
-                    WidgetSpan(
-                      alignment: PlaceholderAlignment.middle,
-                      baseline: TextBaseline.alphabetic,
-                      child: Icon(icon,
-                          size: 12, color: textColor ?? DsfrColorDecisions.textActionHighBlueFrance(context)),
+  Widget build(final context) {
+    return Focus(
+        focusNode: focusNode,
+        canRequestFocus: true,
+        child: Builder(builder: (final context) {
+          final isFocused = Focus.of(context).hasFocus;
+          return DsfrFocusWidget(
+              isFocused: isFocused,
+              child: GestureDetector(
+                onTap: onTap,
+                behavior: HitTestBehavior.opaque,
+                child: DecoratedBox(
+                  decoration: ShapeDecoration(
+                    color: backgroundColor ?? DsfrColorDecisions.backgroundActionLowBlueFrance(context),
+                    shape: const StadiumBorder(),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 8),
+                    child: Text.rich(
+                      TextSpan(
+                        children: [
+                          if (icon != null) ...[
+                            WidgetSpan(
+                              alignment: PlaceholderAlignment.middle,
+                              baseline: TextBaseline.alphabetic,
+                              child: Icon(icon,
+                                  size: 12, color: textColor ?? DsfrColorDecisions.textActionHighBlueFrance(context)),
+                            ),
+                            const WidgetSpan(
+                              child: SizedBox(width: DsfrSpacings.s1v),
+                            ),
+                          ],
+                          label,
+                        ],
+                      ),
+                      style: DsfrTextStyle.bodyXs(
+                          color: textColor ?? DsfrColorDecisions.textActionHighBlueFrance(context)),
                     ),
-                    const WidgetSpan(
-                      child: SizedBox(width: DsfrSpacings.s1v),
-                    ),
-                  ],
-                  label,
-                ],
-              ),
-              style: DsfrTextStyle.bodyXs(color: textColor ?? DsfrColorDecisions.textActionHighBlueFrance(context)),
-            ),
-          ),
-        ),
-      );
+                  ),
+                ),
+              ));
+        }));
+  }
 }
