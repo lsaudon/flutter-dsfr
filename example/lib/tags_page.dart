0f8d6cb968da8cace48fdbecd9d202ced81e56a9
Jasmine Lebert <lebert.jasmine@gmail.com>
2025-04-03 17:47:53 +0200

    feat(tag): ajout des exemples de groupes (#61)

--- example/lib/tags_page.dart
+++ example/lib/tags_page.dart
@@ -1,3 +1,4 @@
+import 'package:example/mise_en_page/page_sub_section.dart';
 import 'package:flutter_dsfr/flutter_dsfr.dart';
 import 'package:example/page_item.dart';
 import 'package:flutter/material.dart';
@@ -21,8 +22,12 @@ class _TagsPageState extends State<TagsPage> {
   bool _isTag2Selected = true;
   bool _isTag3Selected = true;
   bool _isTag4Selected = true;
+  bool _isTag5Selected = false;
+  bool _isTag6Selected = false;
   bool _showDeletableTagSm = true;
   bool _showDeletableTagMd = true;
+
+  final List _groupTagSupprimable = ['Tag 1', 'Tag 2', 'Tag 3', 'Tag 4', 'Tag 5', 'Tag 6'];
 
   @override
   Widget build(final context) => SingleChildScrollView(
@@ -129,6 +134,190 @@ class _TagsPageState extends State<TagsPage> {
                 label: TextSpan(text: 'Tag supprimable'),
                 onDelete: () => setState(() => _showDeletableTagMd = false),
               ),
+            PageSubSection(title: 'Groupe de tags cliquables', children: [
+              DsfrGroup<DsfrTag>(
+                direction: Direction.horizontal,
+                children: [
+                  DsfrTag.sm(
+                    label: TextSpan(text: 'Tag 1'),
+                    onTap: () => print('Tag 1 cliquable par défaut'),
+                  ),
+                  DsfrTag.sm(
+                    label: TextSpan(text: 'Tag 2'),
+                    onTap: () => print('Tag 2 cliquable par défaut'),
+                  ),
+                  DsfrTag.sm(
+                    label: TextSpan(text: 'Tag 3'),
+                    onTap: () => print('Tag 3 cliquable par défaut'),
+                  ),
+                  DsfrTag.sm(
+                    label: TextSpan(text: 'Tag 4'),
+                    onTap: () => print('Tag 4 cliquable par défaut'),
+                  ),
+                  DsfrTag.sm(
+                    label: TextSpan(text: 'Tag 5'),
+                    onTap: () => print('Tag 5 cliquable par défaut'),
+                  ),
+                  DsfrTag.sm(
+                    label: TextSpan(text: 'Tag 6'),
+                    onTap: () => print('Tag 6 cliquable par défaut'),
+                  ),
+                  DsfrTag.sm(
+                    label: TextSpan(text: 'Tag 7'),
+                    onTap: () => print('Tag 7 cliquable par défaut'),
+                  ),
+                  DsfrTag.sm(
+                    label: TextSpan(text: 'Tag avec icone'),
+                    icon: DsfrIcons.systemArrowRightLine,
+                    onTap: () => print('Tag cliquable par défaut avec icone'),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              DsfrGroup<DsfrTag>(
+                direction: Direction.horizontal,
+                children: [
+                  DsfrTag.md(
+                    label: TextSpan(text: 'Tag 1'),
+                    onTap: () => print('Tag 1 cliquable par défaut'),
+                  ),
+                  DsfrTag.md(
+                    label: TextSpan(text: 'Tag 2'),
+                    onTap: () => print('Tag 2 cliquable par défaut'),
+                  ),
+                  DsfrTag.md(
+                    label: TextSpan(text: 'Tag 3'),
+                    onTap: () => print('Tag 3 cliquable par défaut'),
+                  ),
+                  DsfrTag.md(
+                    label: TextSpan(text: 'Tag 4'),
+                    onTap: () => print('Tag 4 cliquable par défaut'),
+                  ),
+                  DsfrTag.md(
+                    label: TextSpan(text: 'Tag 5'),
+                    onTap: () => print('Tag 5 cliquable par défaut'),
+                  ),
+                  DsfrTag.md(
+                    label: TextSpan(text: 'Tag 6'),
+                    onTap: () => print('Tag 6 cliquable par défaut'),
+                  ),
+                  DsfrTag.md(
+                    label: TextSpan(text: 'Tag 7'),
+                    onTap: () => print('Tag 7 cliquable par défaut'),
+                  ),
+                  DsfrTag.md(
+                    label: TextSpan(text: 'Tag avec icone'),
+                    icon: DsfrIcons.systemArrowRightLine,
+                    onTap: () => print('Tag cliquable par défaut avec icone'),
+                  ),
+                ],
+              ),
+            ]),
+            PageSubSection(
+              title: 'Groupe de tags selectionnables',
+              children: [
+                DsfrGroup<DsfrTag>(
+                  direction: Direction.horizontal,
+                  children: [
+                    DsfrTag.sm(
+                      label: TextSpan(text: 'Tag 1'),
+                      isSelected: _isTag1Selected,
+                      onSelectionChanged: (final isSelected) => setState(() => _isTag1Selected = isSelected),
+                    ),
+                    DsfrTag.sm(
+                      label: TextSpan(text: 'Tag 2'),
+                      isSelected: _isTag2Selected,
+                      onSelectionChanged: (final isSelected) => setState(() => _isTag2Selected = isSelected),
+                    ),
+                    DsfrTag.sm(
+                      label: TextSpan(text: 'Tag 3'),
+                      isSelected: _isTag3Selected,
+                      onSelectionChanged: (final isSelected) => setState(() => _isTag3Selected = isSelected),
+                    ),
+                    DsfrTag.sm(
+                      label: TextSpan(text: 'Tag 4'),
+                      isSelected: _isTag4Selected,
+                      onSelectionChanged: (final isSelected) => setState(() => _isTag4Selected = isSelected),
+                    ),
+                    DsfrTag.sm(
+                      label: TextSpan(text: 'Tag 5'),
+                      isSelected: _isTag5Selected,
+                      onSelectionChanged: (final isSelected) => setState(() => _isTag5Selected = isSelected),
+                    ),
+                    DsfrTag.sm(
+                      label: TextSpan(text: 'Tag 6'),
+                      isSelected: _isTag6Selected,
+                      onSelectionChanged: (final isSelected) => setState(() => _isTag6Selected = isSelected),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                DsfrGroup<DsfrTag>(
+                  direction: Direction.horizontal,
+                  children: [
+                    DsfrTag.md(
+                      label: TextSpan(text: 'Tag 1'),
+                      isSelected: _isTag1Selected,
+                      onSelectionChanged: (final isSelected) => setState(() => _isTag1Selected = isSelected),
+                    ),
+                    DsfrTag.md(
+                      label: TextSpan(text: 'Tag 2'),
+                      isSelected: _isTag2Selected,
+                      onSelectionChanged: (final isSelected) => setState(() => _isTag2Selected = isSelected),
+                    ),
+                    DsfrTag.md(
+                      label: TextSpan(text: 'Tag 3'),
+                      isSelected: _isTag3Selected,
+                      onSelectionChanged: (final isSelected) => setState(() => _isTag3Selected = isSelected),
+                    ),
+                    DsfrTag.md(
+                      label: TextSpan(text: 'Tag 4'),
+                      isSelected: _isTag4Selected,
+                      onSelectionChanged: (final isSelected) => setState(() => _isTag4Selected = isSelected),
+                    ),
+                    DsfrTag.md(
+                      label: TextSpan(text: 'Tag 5'),
+                      isSelected: _isTag5Selected,
+                      onSelectionChanged: (final isSelected) => setState(() => _isTag5Selected = isSelected),
+                    ),
+                    DsfrTag.md(
+                      label: TextSpan(text: 'Tag 6'),
+                      isSelected: _isTag6Selected,
+                      onSelectionChanged: (final isSelected) => setState(() => _isTag6Selected = isSelected),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            PageSubSection(
+              title: 'Groupe de tags supprimables',
+              children: [
+                DsfrGroup<DsfrTag>(
+                  direction: Direction.horizontal,
+                  children: [
+                    if (_groupTagSupprimable.isNotEmpty)
+                      for (var tag in _groupTagSupprimable)
+                        DsfrTag.sm(
+                          label: TextSpan(text: tag),
+                          onDelete: () => setState(() => _groupTagSupprimable.remove(tag)),
+                        ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                DsfrGroup<DsfrTag>(
+                  direction: Direction.horizontal,
+                  children: [
+                    if (_groupTagSupprimable.isNotEmpty)
+                      for (var tag in _groupTagSupprimable)
+                        DsfrTag.md(
+                          label: TextSpan(text: tag),
+                          onDelete: () => setState(() => _groupTagSupprimable.remove(tag)),
+                        ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 200),
           ],
         ),
       );
